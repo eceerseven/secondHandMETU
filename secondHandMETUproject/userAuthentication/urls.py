3163c546a3ca6cbbad69f2b3a57e0ce1acd7d32a
ece erseven <48152149+eceerseven@users.noreply.github.com>
2023-12-14 05:23:10 +0200

    Add files via upload

---
+++ secondHandMETUproject/userAuthentication/urls.py
@@ -0,0 +1,16 @@
+#authentication/urls.py
+from django.contrib.auth.decorators import login_required
+from .views import register, user_login, user_logout, user_profile, dashboard
+from django.urls import path
+
+app_name = 'userAuthentication'
+urlpatterns = [
+    path('register/', register, name='register'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
+    path('profile/', login_required(user_profile), name='profile'),
+    path('dashboard/', dashboard, name='dashboard'),
+    path('profile/', user_profile, name='user_profile'),
+    path('logout/', user_logout, name='logout'),
+]
+
